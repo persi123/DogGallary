d8b78f1f1d3d56348efd861baf9b88ddeb74a7e9
prashant tongra <40728348+persi123@users.noreply.github.com>
2020-03-21 13:22:00 +0530

    deploy

--- client/src/store.js
+++ client/src/store.js
@@ -8,23 +8,9 @@ let store;
 // Enable redux dev tool only in development mode
 if (process.env.NODE_ENV === "development") {
   const composeEnhancers = window.REDUX_DEVTOOLS_EXTENSION_COMPOSE || compose;
-  store = createStore(
-    reducers,
-    compose(
-      applyMiddleware(thunk),
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-  );
+  store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 } else {
-  store = createStore(
-    reducers,
-    compose(
-      applyMiddleware(thunk),
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-  );
+  store = createStore(reducers, applyMiddleware(thunk));
 }
 
 export default store;
